fc0f4ea36ea49076556261aef716ccf13b560a77
David Fleming <tonage72@gmail.com>
2024-01-23 18:19:50 -0500

    done

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,36 @@
 const markAllRead = document.querySelector('.mark-all-read');
 const allCardUnread = document.querySelectorAll('.card-unread');
+const countNumber = document.querySelector('.count');
+let mark = 1;
 
 markAllRead.addEventListener("click", markRead);
+let currentCount = allCardUnread.length;
+countNumber.textContent = allCardUnread.length;
 
 function markRead() {
+	
+	if (mark == 2) {
+		markAllRead.textContent = "Mark all as read";
+		countNumber.textContent = currentCount;
+		mark = 1;
+	} else {
+		markAllRead.textContent = "Undo";
+		countNumber.textContent = currentCount - allCardUnread.length;
+		mark = 2;
+	}
+
 	for (var i = 0; i < allCardUnread.length; i++) {
+		allCardUnread[i].classList.toggle('card-transform');
 		allCardUnread[i].classList.toggle('card-unread');
+	}
+
+	
+	
+	setTimeout (clearClass, 1000);
+}
+
+function clearClass() {
+	for (var i = 0; i < allCardUnread.length; i++) {
+		allCardUnread[i].classList.remove('card-transform');
 	}
 }
